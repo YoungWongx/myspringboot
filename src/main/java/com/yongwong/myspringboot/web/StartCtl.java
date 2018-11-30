80c34ec55e38907c5f6dee3f19843c0fa39e22b6
YoungWongx <youngwongms@gmail.com>
2018-11-30 17:03:14 +0800

    dfdfdf

---
+++ src/main/java/com/yongwong/myspringboot/web/StartCtl.java
@@ -0,0 +1,20 @@
+package com.yongwong.myspringboot.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @AUTHOR hugowong
+ * @DATE 2018/11/30
+ * @VERSION 1.0.0
+ * @DESCRIPTION
+ */
+@RestController
+@RequestMapping("/")
+public class StartCtl {
+
+  @RequestMapping("/start")
+  public String start(){
+    return "welcome to jenkins";
+  }
+}
